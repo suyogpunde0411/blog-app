293c9fc02c51773657ff47cb8ec247cb5cdef0c4
suyogpunde0411 <163162554+suyogpunde0411@users.noreply.github.com>
2026-05-13 09:03:07 +0530

    ..

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,3 +6,24 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
 })
+
+
+
+// sudo nano /etc/nginx/sites-available/default
+
+// server {
+//     listen 80;
+//     server_name _;
+
+//     location / {
+//         root /home/ubuntu/blog-app/client/dist;
+//         index index.html;
+//         try_files $uri $uri/ /index.html;
+//     }
+
+//     location /api/ {
+//         proxy_pass http://172.31.x.x:5000;  # backend private IP
+//         proxy_http_version 1.1;
+//         proxy_set_header Host $host;
+//     }
+// }
